3860549dda73383e35890a32d6a14bf22af020b4
Stanislav Sergienya <skilful522@gmail.com>
2018-04-29 13:55:06 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,15 @@
 package com.company;
 
+import com.company.bouquet.Bouquet;
+import com.company.rose.RoseFlower;
+import com.company.snowdrop.SnowdropFlower;
+import com.company.tulip.TulipFlower;
+
 public class Main {
     public static void main(String[] args) {
 
-        RoseFlower roseFlower = new RoseFlower("Красный",20,"Сегодняшние");
-        TulipFlower tulipFlower = new TulipFlower("Жёлтый",15,"Вчерашний");
+        RoseFlower roseFlower = new RoseFlower("Красный",20,"Сегодняшние",25);
+        TulipFlower tulipFlower = new TulipFlower("Жёлтый",15,"Вчерашний", 15);
         SnowdropFlower snowdropFlower = new SnowdropFlower("Чёрный",30,"Гнилой", 0);
         Bouquet bouquet = new Bouquet(roseFlower,tulipFlower,snowdropFlower);
         System.out.println(bouquet);
